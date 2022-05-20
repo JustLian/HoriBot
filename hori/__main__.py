14bdc52ac7bb701cf58fe68b90b363a36529fc8f
JustLian <why.lians.mail@gmail.com>
2022-05-21 08:39:25 +0900

    Invite message

--- hori/__main__.py
+++ hori/__main__.py
@@ -1,9 +1,9 @@
 import asyncio
 import os
 import nextcord
-from hori import db
+from hori import db, CColour
 from termcolor import colored
-from nextcord import *
+from nextcord import Embed
 from nextcord.ext import commands
 
 
@@ -21,7 +21,20 @@ async def on_ready():
 
 
 @bot.event
-async def on_guild_join(guild):
+async def on_guild_join(guild: nextcord.Guild):
+    em = Embed(title='Thanks for inviting!',
+               description="Hi! Thankyou for inviting me :> Setup your free 24/7 radio using /setup_radio commad!", colour=CColour.light_orange)
+    em.add_field(name='Developer',
+                 value='https://github.com/JustLian', inline=False)
+    em.add_field(name='HoriBot on Github',
+                 value='https://github.com/JustLian/HoriBot', inline=False)
+    em.add_field(name='MarinBot on Github',
+                 value='https://github.com/JustLian/MarinBot', inline=False)
+    em.set_thumbnail(url='attachment://happy-5.gif')
+    if guild.system_channel is None:
+        channel = max([c for c in guild.channels if c.permissions_for(
+            guild.self_role).send_messages == True], key=lambda x: x.position)
+        await channel.send(embed=em, file=nextcord.File('./assets/emotes/happy-5.gif'))
     db.create_server(guild.id)
 
 
@@ -30,13 +43,13 @@ async def on_guild_remove(guild):
     db.delete_server(guild.id)
 
 
-print('Loading ', colored('Hori', 'magenta'), '...', sep='')
-for ext in os.listdir('./hori/cogs/'):
-    if ext.endswith('.py'):
-        bot.load_extension(f'hori.cogs.{ext[:-3]}')
-        print('Loaded extension',
-              colored(ext, 'blue', 'on_yellow'))
-print('Extensions loaded')
+if __name__ == '__main__':
+    print('Loading ', colored('Hori', 'magenta'), '...', sep='')
+    for ext in os.listdir('./hori/cogs/'):
+        if ext.endswith('.py'):
+            bot.load_extension(f'hori.cogs.{ext[:-3]}')
+            print('Loaded extension',
+                  colored(ext, 'blue', 'on_yellow'))
+    print('Extensions loaded')
 
-
-bot.run(_token)
+    bot.run(_token)
