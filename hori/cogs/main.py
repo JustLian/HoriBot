23090624b101e8d416fc8d11cf94ba12b49df483
JustLian <why.lians.mail@gmail.com>
2022-05-22 12:03:26 +0900

    Basic /lyrics command

--- hori/cogs/main.py
+++ hori/cogs/main.py
@@ -1,7 +1,13 @@
-from nextcord import Interaction, Colour, SlashOption
+from nextcord import Interaction, Embed, SlashOption
+import lyricsgenius
 import nextcord
 from nextcord.ext import commands
 from hori import GUILDS, CColour
+
+
+with open('./secrets/genius_token', 'r') as f:
+    _genius_token = f.read().strip()
+genius = lyricsgenius.Genius(_genius_token)
 
 
 class Main(commands.Cog):
@@ -25,6 +31,25 @@ class Main(commands.Cog):
             em.add_field(name=field, value=fields[field], inline=False)
 
         await inter.edit_original_message(embed=em, file=nextcord.File('./assets/emotes/happy-2.png'))
+
+    @nextcord.slash_command('lyrics', 'Find song lyrics', GUILDS)
+    async def cmd_lyrics(self, inter: Interaction, title: str = SlashOption('')):
+        await inter.response.defer()
+
+        song = genius.search_song(title)
+
+        if song is None:
+            em = Embed(title='Nothing found',
+                       description="I couldn't find a song with that title", colour=CColour.brown)
+            em.set_thumbnail(url='attachment://sad-3.png')
+            await inter.edit_original_message(embed=em, file=nextcord.File('./assets/emotes/sad-3.png'))
+            return
+
+        em = Embed(title=song.title, description="Lyrics:\n" + 'Lyrics'.join(song.lyrics.split('Lyrics')[1:]),
+                   colour=CColour.light_orange)
+        em.set_footer(text=song.artist)
+        em.set_thumbnail(url=song.song_art_image_thumbnail_url)
+        await inter.edit_original_message(embed=em)
 
 
 def setup(bot):
