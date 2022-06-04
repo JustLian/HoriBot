aea318cb38f9231989b5e161a1cdbbc83955fbc7
JustLian <why.lians.mail@gmail.com>
2022-06-04 22:26:46 +0900

    doesn't work...

--- hori/__main__.py
+++ hori/__main__.py
@@ -1,10 +1,13 @@
 import asyncio
 import logging
 import os
+import traceback
 import nextcord
 from hori import db, CColour, PRESENCE_LOOP_COOLDOWN
+from hori.cogs import radio
 from termcolor import colored
 from nextcord import Embed
+from nextwave import Player
 from nextcord.ext import commands, tasks
 import topgg
 
@@ -21,6 +24,7 @@ with open('./secrets/bot_token', 'r') as f:
 with open('./secrets/topgg_token', 'r') as f:
     _topgg_token = f.read().strip()
 restart_in = None
+queues = []
 
 bot = commands.Bot(command_prefix='hori/', intents=nextcord.Intents.all())
 
@@ -39,6 +43,59 @@ async def cmd_restart_presence(ctx: commands.Context, minutes: int):
     if (await bot.application_info()).owner.id == ctx.author.id:
         restart_in = minutes
         await ctx.reply('done')
+
+
+@bot.command('restart_bot')
+async def cmd_restart_bot(ctx: commands.Context):
+    if (await bot.application_info()).owner.id == ctx.author.id:
+        global queues, restart_in
+        restart_in = 'restarting'
+        print('Getting ready to reload bot...')
+        queues, waiting, done = [], 0, 0
+
+        async def fetch_guild_before(guild):
+            nonlocal done
+            vc: Player = guild.voice_client
+            await guild.me.edit(nick=f'{bot.user.name} | Reloading!')
+            if not vc.queue.is_empty and db.get_server(vc.guild.id)['radio_enabled'] == 0:
+                queues.append(vc.channel, list(vc.queue))
+            await vc.stop()
+            done += 1
+
+        async def fetch_guild_after(d):
+            await d[0].connect()
+            await d[0].guild.voice_client.queue.extend(d[1])
+
+        for g in bot.guilds:
+            if g.voice_client is not None:
+                bot.loop.create_task(fetch_guild_before(g))
+                waiting += 1
+
+        print('Waiting...')
+        while waiting != done:
+            await asyncio.sleep(1)
+        print('Reloading all extensions!')
+
+        for ext in bot.extensions:
+            print(f'Reloading extension\t{ext}')
+            bot.reload_extension(ext)
+        print(f'Everything is done! Restarting music player')
+
+        restart_in = None
+        for d in queues:
+            bot.loop.create_task(fetch_guild_after(d))
+
+        print(f'Restarting radio')
+        for guild_id in db.get_servers():
+            if db.get_server(guild_id)['radio_enabled'] == 0:
+                continue
+            try:
+                await asyncio.create_task(radio.start_radio(bot, guild_id))
+            except Exception as e:
+                print(
+                    f'Error occurred in start_radio({bot}, {guild_id}): {e}')
+                traceback.print_exception(e)
+        print('Finished reloading!')
 
 
 @bot.event
@@ -78,7 +135,9 @@ async def on_guild_remove(guild):
 @tasks.loop(seconds=PRESENCE_LOOP_COOLDOWN * 2)
 async def presence_loop():
     global restart_in
-    if restart_in is not None:
+    if restart_in is 'restarting':
+        await bot.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.playing, name='Restarting bot!'), status=nextcord.Status.idle)
+    elif restart_in is not None:
         restart_in -= PRESENCE_LOOP_COOLDOWN / 60
         if restart_in < 0:
             restart_in = 0
